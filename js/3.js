662241c499b7fd66d6025df10f82792996f37842
Mauricio <mauricioutn2017@gmail.com>
2023-03-03 03:38:06 -0300

    Ejercicios 1,2 y 3 listos

---
+++ js/3.js
@@ -0,0 +1,7 @@
+const cadena = [prompt("Ingrese una cadena: ")];
+console.log(typeof(cadena));
+while(confirm("Presione cancelar para terminar")!== false){
+    cadena.push (prompt("Ingrese una cadena: "));
+}
+    document.write(cadena)
+
