896e51f76e3dc2cb1607fc9b3277df03cbfde62f
Mauricio <mauricioutn2017@gmail.com>
2023-03-10 10:25:48 -0300

    Resuelto ejercicio 7

--- js/6.js
+++ js/6.js
@@ -1,2 +1,6 @@
 
-document.write('1<br>22<br>333<br>4444<br>55555<br>666666');
+for(let i=0;i<=30;i++){
+    document.write("<br>");
+    for(let j=0;j<=i;j++)
+    document.write(i);
+}
--- js/7.js
+++ js/7.js
@@ -1,3 +1,9 @@
-var test = 5 +7;
-
-document.write(test);
+let num= parseInt(prompt("Ingrese numero:"));
+var piramide
+for(let i=num;i>=1;i--){
+    document.write("<br>");
+    piramide=i;
+    for(let j=i;j>0;j--){
+    document.write(piramide);
+    }
+}
